c21b6d1e8e19701c66761d20f4739f940d0fe493
Jonas Möller <github@uelfte.ninja>
2016-04-25 15:47:47 +0200

    added helper file for dummy data

---
+++ universal/json.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jonas
+ * Date: 25.04.2016
+ * Time: 14:28
+ */
+
+$array = array(
+    'status' => false,
+    'question' => 'Does {{firstname}} smell very weird?',
+    'firstname' => 'Jonas',
+    'twitter' => 'aledjonesworld',
+    'twitter_owner' => 'simonfieberDE',
+    'true_text' => 'YES!',
+    'true_long' => 'you smell very weird',
+    'false_text' => 'Nope.',
+    'false_long' => 'what´s up? You don´t smell weird!',
+    'version' => '1.2'
+);
+$json = json_encode($array);
+file_put_contents('lib/data/content.json', $json);
