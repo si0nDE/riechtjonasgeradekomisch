66ba4d42bf182ba83805300bff6dccaa98bb3a3b
Jonas Möller <github@uelfte.ninja>
2016-04-26 17:02:45 +0200

    fixed footer

--- universal/index.php
+++ universal/index.php
@@ -18,9 +18,9 @@ $data = json_decode($data);
 		<div id="tweet"><a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $m->render('{{url}}',$data);?>" data-text="<?php if ($data->status){echo $m->render('Yo {{firstname}}, {{true_long}} (@{{twitter}})', $data);}else{echo $m->render('Yo {{firstname}}, {{false_long}} (@{{twitter}})', $data);} ?>" data-via="<?php echo $m->render('{{twitter_owner}}', $data)?>" data-size="large">Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></div>
 		<div id="top"><?php echo $m->render($data->question,$data) ?></div>
 		<div id="main"><?php if ($data->status){echo $m->render('{{true_text}}', $data);}else{echo $m->render('{{false_text}}', $data);} ?></div>
+		<footer>
+			Made by <a target="_blank" href="https://twitter.com/simonfieberDE">@simonfieberDE</a><br />
+			<a target="_blank" href="https://github.com/simonfieberDE/riechtjonasgeradekomisch">GitHub Source</a> &middot; <?php echo $m->render('v{{version}}',$data)?>
+		</footer>
 	</body>
-	<footer>
-		Made by <a target="_blank" href="https://twitter.com/simonfieberDE">@simonfieberDE</a><br />
-		<a target="_blank" href="https://github.com/simonfieberDE/riechtjonasgeradekomisch">GitHub Source</a> &middot; <?php echo $m->render('v{{version}}',$data)?>
-	</footer>
 </html>
