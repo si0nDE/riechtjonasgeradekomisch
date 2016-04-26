2de57ca5ac17183f9259ee3128617c494308146e
Jonas Möller <github@uelfte.ninja>
2016-04-26 15:47:24 +0200

    Admin-Interface 0.2
    Basic design changes, new json.php helper with data for admin core
    merged in one single file, something with fonts

--- universal/admin/index.php
+++ universal/admin/index.php
@@ -1,27 +1,39 @@
 <?php
 // Management Interface
+require_once '../lib/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+$data = json_decode(file_get_contents('../lib/data/content.json'));
+$m = new Mustache_Engine();
+echo $m->render('',$data);
+//exit;
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{title}}</title>
-        <link rel="stylesheet/less" type="text/less" href="css/less/bootstrap.less">
-        <link rel="stylesheet" type="text/css" href="css/admin.css">
-        <script>less = { env: 'development'};</script>
-        <script src="../js/less.js"></script>
-        <script>//less.watch();</script>
-    </head>
-        <body>
-            <div class="container">
-                <form class="form-admin">
-                    <h2 class="form-admin-heading">{{question}}</h2>
-                    <label for="inputQuestion" class="sr-only">{{question_lable}}</label>
-                    <input name="inputQuestion" class="form-control" type="text" value="{{question}}" required autofocus>
-                    <button class="btn btn-lg btn-success btn-block" type="submit"><span class="glyphicon glyphicon-ok"></span> {{btn_lable}}</button>
-                </form>
-            </div>
-        </body>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{admin.title}}</title>
+    <link rel="stylesheet/less" type="text/less" href="css/less/bootstrap.less">
+    <link rel="stylesheet" type="text/css" href="css/admin.css">
+    <script>less = { env: 'development'};</script>
+    <script src="../js/less.js"></script>
+    <script>//less.watch();</script>
+</head>
+<body>
+<div class="container">
+    <form class="form-admin">
+        <h2 class="form-admin-heading">{{question}}</h2><br>
+        <div class="form-group">
+            <label for="question">{{admin.question_lable}}</label>
+            <input name="question" class="form-control" type="text" value="{{question}}" required>
+        </div>
+        <div class="form-group">
+            <label for="firstname">{{admin.firstname_lable}}</label>
+            <input name="firstname" class="form-control" type="text" value="{{firstname}}" required>
+        </div>
+        <button class="btn btn-lg btn-success btn-block" type="submit"><span class="glyphicon glyphicon-ok"></span>&nbsp;{{admin.btn_lable}}</button>
+    </form>
+</div>
+</body>
 </html>
--- universal/json.php
+++ universal/json.php
@@ -1,10 +1,4 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Jonas
- * Date: 25.04.2016
- * Time: 14:28
- */
+﻿<?php
 
 $array1 = array(
     'status' => true,
@@ -17,17 +11,16 @@ $array1 = array(
     'false_text' => 'Nope.',
     'false_long' => 'what´s up? You don´t smell weird!',
     'version' => '1.2',
-    'url' => 'http://riechtjonasgeradekomisch.com'
+    'url' => 'http://riechtjonasgeradekomisch.com',
+    'admin' => array(
+        'question_lable' => 'Question:',
+        'btn_lable' => 'Save',
+        'title' => 'admin@riechtjonasgeradekomisch.com'
+    )
 );
 
-$array2 = array(
-    'question_lable' => 'Question:',
-    'btn_lable' => 'Save'
-);
+
 $json = json_encode($array1);
 file_put_contents('lib/data/content.json', $json);
 echo "Done: 1\n";
-$json = json_encode($array2);
-file_put_contents('lib/data/admin.json', $json);
-echo "Done: 2\n";
 echo "STOP";
