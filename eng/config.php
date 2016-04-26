faad178f14ced82d561aa7980a2047c8ceb3f4a4
Jonas Möller <github@uelfte.ninja>
2016-04-26 17:22:18 +0200

    Update $twitterowner

--- eng/config.php
+++ eng/config.php
@@ -15,11 +15,11 @@
 
 	$firstname			=	'Jonas';
 	$twittername		=	'aledjonesworld';	/* without @ */
-	$twitterowner		=	'simonfieberDE';	/* without @ */
+	$twitterowner		=	'manchmalfieber';	/* without @ */
 	
 	$true				=	'YES!';
 	$true_text			=	'you smell very weird!';
 	$false				=	'Nope.';
 	$false_text			=	'what´s up? You don´t smell weird!';
 	
-	$version			=	'1.1.1';
+	$version			=	'1.1.2';
