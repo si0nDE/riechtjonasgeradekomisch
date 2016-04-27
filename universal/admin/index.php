bcc5e5dcf9c76e253573928996e43812c23b3a0e
Jonas Möller <github@uelfte.ninja>
2016-04-27 14:16:58 +0200

    finished admin interface, saving, error handling and form issues

--- universal/admin/index.php
+++ universal/admin/index.php
@@ -2,16 +2,19 @@
 session_start();
 require_once '../lib/Mustache/Autoloader.php';
 Mustache_Autoloader::register();
-$data = json_decode(file_get_contents('../lib/data/content.json'));
 $m = new Mustache_Engine(array(
     'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/views',array('extension' => '.html'))
 ));
-if(isset($_SESSION['success'])){
-    if($_SESSION['success']) {
-        $data->admin->save = true;
-    }
+$data = json_decode(file_get_contents('../lib/data/content.json'));
+if (isset($_SESSION['error'])){
+    if($_SESSION['error']){
+    $data->admin->error = true;
+    $data->admin->error_msg = $_SESSION['error_msg'];}
+}
+if (isset($_SESSION['success'])){
+    if ($_SESSION['success']){
+    $data->admin->save = true;}
 }
 echo $m->render('admin', $data);
 session_destroy();
-session_start();
 ?>
--- universal/admin/save.php
+++ universal/admin/save.php
@@ -1,42 +1,59 @@
 <?php
-session_start();
-$array = array(
-    'core' => array(
-        'status' => true,
-        'question' => $_POST['core.question'],
-        'firstname' => $_POST['core.firstname'],
-        'twitter' => array(
-            'owner' => $_POST['core.twitter.owner'],
-            'nick' => $_POST['core.twitter.nick']
-        ),
-        'true' => array(
-            'text' => $_POST['core.true.text'],
-            'long' => $_POST['core.true.long']
-        ),
-        'false' => array(
-            'text' => $_POST['core.false.text'],
-            'long' => $_POST['core.false.long']
-        ),
-        'url' => $_POST['core.url'],),
-    'admin' => array(
-        'question_label' => 'Frage:',
-        'btn_label' => 'Speichern',
-        'title' => 'admin@riechtjonasgeradekomisch.com',
-        'firstname_label' => 'Vorname:',
-        'twitter_label' => 'Twitter-Benutzername (ohne @):',
-        'twitter_owner_label' => 'Twitter-Benutzername von dir (ohne @):',
-        'true_text_label' => 'Kurze Antwort, wenn zutreffend:',
-        'true_long_label' => 'Text für Tweet, wenn zutreffend:',
-        'false_text_label' => 'Kurze Antwort, wenn nicht zutreffend:',
-        'false_long_label' => 'Text für Tweet, wenn nicht zutreffend:',
-        'url_label' => 'Domain für Tweet (z.B. http://riechtjonasgeradekomisch.com)',
-        'save_label' => 'Daten gespeichert'
-    )
-);
-
-
-$json = json_encode($array);
-file_put_contents('../lib/data/content.json', $json);
-$_SESSION['success'] = true;
-//header('Location: index.php');
-print_r($_POST);
+if (!empty($_POST)){
+    $array = array(
+        'core' => array(
+            'status' => true,
+            'question' => $_POST['core_question'],
+            'firstname' => $_POST['core_firstname'],
+            'twitter' => array(
+                'owner' => $_POST['core_twitter_owner'],
+                'nick' => $_POST['core_twitter_nick']
+            ),
+            'true' => array(
+                'text' => $_POST['core_true_text'],
+                'long' => $_POST['core_true_long']
+            ),
+            'false' => array(
+                'text' => $_POST['core_false_text'],
+                'long' => $_POST['core_false_long']
+            ),
+            'url' => $_POST['core_url'],
+            'version' => '1.5.1'),
+        'admin' => array(
+            'question_label' => 'Frage:',
+            'btn_label' => 'Speichern',
+            'title' => 'admin@riechtjonasgeradekomisch.com',
+            'firstname_label' => 'Vorname:',
+            'twitter_label' => 'Twitter-Benutzername (ohne @):',
+            'twitter_owner_label' => 'Twitter-Benutzername von dir (ohne @):',
+            'true_text_label' => 'Kurze Antwort, wenn zutreffend:',
+            'true_long_label' => 'Text für Tweet, wenn zutreffend:',
+            'false_text_label' => 'Kurze Antwort, wenn nicht zutreffend:',
+            'false_long_label' => 'Text für Tweet, wenn nicht zutreffend:',
+            'url_label' => 'Domain für Tweet (z.B. http://riechtjonasgeradekomisch.com)',
+            'save_label' => 'Daten gespeichert'
+        )
+    );
+    $json = json_encode($array);
+    $save = file_put_contents('../lib/data/content.json', $json);
+    if ($save === false) {
+        session_start();
+        $error = error_get_last();
+        $_SESSION['error'];
+        $_SESSION['error_msg'] = "Code <b>".$error['type']."</b>: Error in line <b>".$error['line']."</b>!<br>Message: ".$error['message'];
+        header('Location: index.php');
+        exit(1);
+    }else{
+        session_start();
+        $_SESSION['success'] = true;
+        header('Location: index.php');
+        exit;
+    }
+}else{
+    session_start();
+    $error = error_get_last();
+    $_SESSION['error'];
+    $_SESSION['error_msg'] = "Code <b>".$error['type']."</b>: Error in line <b>".$error['line']."</b>!<br>Message: ".$error['message'];
+    header('Location: index.php');
+    exit(1);
+}
